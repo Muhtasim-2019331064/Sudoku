121d6d54cf1968b3288f64aa9dfa5d21307da44d
Muhtasim-2019331064 <muhtasim.ahmed.bhuiyan@gmail.com>
2021-11-22 11:43:48 +0600

    initial commit

---
+++ sudoku_naive_solution.py
@@ -0,0 +1,72 @@
+# Naive sudoku solution using backtracking.
+
+def print_board(board):
+    print()
+
+    for r in range(9):
+        for c in range(9):
+            print(board[r][c], end=" ")
+
+            if c % 3 == 2 and c != 8:
+                print("| ", end="")
+        
+        print("")
+
+        if r % 3 == 2 and r != 8:
+            print("-" * 21)
+    
+    print()
+
+def is_present_in_row(board, row, candidate):
+    for num in board[row]:
+        if num == candidate:
+            return True
+    
+    return False
+
+def is_present_in_column(board, column, candidate):
+    for c in range(9):
+        if board[c][column] == candidate:
+            return True
+    
+    return False
+
+def is_present_in_box(board, row, column, candidate):
+    box_row = row // 3
+    box_column = column // 3
+
+    for r in range(box_row*3, box_row*3 + 3):
+        for c in range(box_column*3, box_column*3 + 3):
+            if board[r][c] == candidate:
+                return True
+
+    return False
+
+def solve(board, empty_cells, index):
+    if index == len(empty_cells):
+        print_board(board)
+        return
+
+    row, column = empty_cells[index]
+
+    for candidate in range(1, 10):
+        if not is_present_in_row(board, row, candidate) and not is_present_in_column(board, column, candidate) and not is_present_in_box(board, row, column, candidate):
+            board[row][column] = candidate
+            solve(board, empty_cells, index+1)
+            board[row][column] = 0
+    
+
+board = []
+
+for cnt in range(9):
+    row = [ int(i) for i in input().split() ]
+    board.append(row)
+
+empty_cells = []
+
+for r in range(9):
+    for c in range(9):
+        if board[r][c] == 0:
+            empty_cells.append([r, c])
+
+solve(board, empty_cells, 0)
